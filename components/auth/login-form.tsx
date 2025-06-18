4572b246f818bfcf38a799a994d89817bfc72071
Jatesh Kumar <jateshkumar@mac.home>
2025-06-18 00:12:14 -0400

    SCRUM-38: Added login form with form success and error components

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../shopnext/components/ui/form"
+import {zodResolver} from '@hookform/resolvers/zod'
+import { AuthCard } from "../shopnext/components/auth/auth-card"
+import { LoginSchema } from "@/types/login-schema"
+import * as z from 'zod'
+import { Input } from "../shopnext/components/ui/input"
+import { Button } from "../shopnext/components/ui/button"
+import Link from "next/link"
+import { emailSignIn } from "@/server/actions/email-signin"
+import { useAction } from "next-safe-action/hooks"
+import { cn } from "@/lib/utils"
+import { useState } from "react"
+
+
+
+
+export const LoginForm = () => {
+    const form = useForm({
+        resolver : zodResolver(LoginSchema),
+        defaultValues : {
+            email : "",
+            password : "",
+        },
+    });
+
+    const[error, setError] = useState("");
+
+    const {execute,status, result} = useAction(emailSignIn,{
+        onSuccess(data){
+         console.log(data);   
+        }
+    })
+
+    const onSubmit = (values : z.infer <typeof LoginSchema>) => {
+        // console.log(values);
+        execute(values)
+    }
+    return(
+        <AuthCard cardTitle="Welcome Back!" backButtonHref="/auth/register" backButtonLabel="Create a new account" showSocials>
+            <div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                     <div>
+                        <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                            <Input {...field} placeholder="shopnext@shopnext.com" type="email" autoComplete="email"></Input>
+                            </FormControl>
+                            <FormDescription />
+                            <FormMessage />
+                        </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                            <Input {...field} placeholder="********" type="password" autoComplete="current-password"></Input>
+                            </FormControl>
+                            <FormDescription />
+                            <FormMessage />
+                        </FormItem>
+                        )}
+                    />
+                    <Button size={'sm'} variant={"link"} asChild>
+                        <Link href="auth/reset">Forgot your password</Link>
+                    </Button>
+
+                     </div>
+                    <Button type="submit" className={cn("w-full my-2", status=== 'executing' ? 'animate-pulse' : "")}>Login</Button>
+                    </form>
+                </Form>
+            </div>
+        </AuthCard>
+    )
+}
