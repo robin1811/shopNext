f6c836b1c2f9124c7a19d389275cc9d0e3054adf
Jatesh Kumar <jateshkumar@52:58:01:0c:7a:87.home>
2025-07-03 14:51:05 -0400

    Admin Product feature/Added the product page for admin

---
+++ app/dashboard/products/columns.tsx
@@ -0,0 +1,102 @@
+"use client"
+
+import { ColumnDef, Row } from "@tanstack/react-table"
+import { MoreHorizontal, PlusCircle } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Image from "next/image"
+import { useAction } from "next-safe-action/hooks"
+import { toast } from "sonner"
+import Link from "next/link"
+
+
+type ProductColumn = {
+  title: string
+  price: number
+  image: string
+  variants: any 
+  id: number
+}
+
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "variants",
+    header: "Variants",
+
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        currency: "USD",
+        style: "currency",
+      }).format(price)
+      return <div className="font-medium text-xs">{formatted}</div>
+    },
+  },
+  {
+    accessorKey: "image",
+    header: "Image",
+    cell: ({ row }) => {
+      const cellImage = row.getValue("image") as string
+      const cellTitle = row.getValue("title") as string
+      return (
+        <div className="">
+          <Image
+            src={cellImage}
+            alt={cellTitle}
+            width={50}
+            height={50}
+            className="rounded-md"
+          />
+        </div>
+      )
+    },
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    // cell: ActionCell,
+    cell: ({row}) =>{
+        const product = row.original
+        return(
+            <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+                <Button variant={"ghost"} className="h-8 w-8 p-0">
+                <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem className="dark:focus:bg-primary focus:bg-primary/50 cursor-pointer">
+                    Edit Product
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                className="dark:focus:bg-destructive focus:bg-destructive/50 cursor-pointer"
+                >
+                Delete Product
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+            </DropdownMenu>
+        )
+    }
+  },
+]
